8d62cd71158bddd56f2ab6f8b50a5d74df0aaa81
Kyle Daniel Saqueton <klsaqueton@student.apc.edu.ph>
2024-03-06 22:47:15 +0800

    1

--- ci4/app/Config/Routes.php
+++ ci4/app/Config/Routes.php
@@ -6,8 +6,6 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
-
-
 use App\Controllers\Pages;
 
 $routes->get('pages', [Pages::class, 'index']);
